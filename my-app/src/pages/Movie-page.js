df564ed07ed2263b226609f84e1143749a54c4bc
Nusrath <nusrathjahan0204@gmail.com>
2025-12-23 13:06:49 +0100

    set some styles for movie card

--- my-app/src/pages/Movie-page.js
+++ my-app/src/pages/Movie-page.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import "./Movie.css";
 function MovieApp() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -11,8 +11,8 @@ function MovieApp() {
         setMovies(data.Search || []);
         setLoading(false);
       })
-      .catch((err) => {
-        console.error("Error fetching movies:", err);
+      .catch((error) => {
+        console.error("Error:", error);
         setLoading(false);
       });
   }, []);
@@ -22,20 +22,14 @@ function MovieApp() {
   return (
     <div>
       <h2>Movie List</h2>
-      <div style={{ display: "flex", flexWrip: "wrap", gap: "16px" }}>
+
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
         {movies.map((movie) => (
-          <div
-            key={movie.imdID}
-            style={{
-              border: "1px solid #ccc",
-              width: "200px",
-              padding: "10px",
-            }}
-          >
+          <div key={movie.imdbID} className="movie-card">
             <img
               src={movie.Poster}
               alt={movie.Title}
-              style={{ width: "100%", height: "250px", objectFit: "cover" }}
+              style={{ width: "100%", height: "230px", objectFit: "cover" }}
             />
             <h4>{movie.Title}</h4>
             <p>Year: {movie.Year}</p>
@@ -45,4 +39,5 @@ function MovieApp() {
     </div>
   );
 }
+
 export default MovieApp;
